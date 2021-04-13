2a009456822f986e1c12af590ed3b00d469d788f
Justin W <justinwei.dev@gmail.com>
2021-04-13 16:39:52 +1200

    added basic visuals

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let myLibrary = [];
 
-const bookcase = document.querySelector("#bookcase")
+const bookcase = document.querySelector("#books")
 
 function Book(title, author, pages, read) {
 
@@ -11,7 +11,13 @@ function Book(title, author, pages, read) {
 }
 
 Book.prototype.info = function() {
-    return (`${this.title} by ${this.author}, ${this.pages} pages, ${(this.read) ? "read":"not read yet"}`)
+    return (`${this.author}
+${this.pages} pages
+${(this.read) ? "Read":"Not read"}`)
+}
+
+Book.prototype.getTitle = function() {
+    return (`${this.title}`)
 }
 
 function addBook(book) {
@@ -20,17 +26,31 @@ function addBook(book) {
 
 function displayBooks() {
     myLibrary.forEach( book => {
+        console.log(book.getTitle());
         console.log(book.info());
-        const newBook = document.createElement('li');
-        newBook.textContent = book.info();
+        const newBook = document.createElement('div');
+        const newBookTitle = document.createElement('div');
+        const newBookInfo = document.createElement('div');
+
+        newBookTitle.textContent = book.getTitle();
+        newBookInfo.textContent = book.info();
+
+        newBookTitle.style.fontWeight = 'bold';
+
+        newBook.classList.add('book');
         bookcase.appendChild(newBook);
+        newBook.appendChild(newBookTitle);
+        newBook.appendChild(newBookInfo);
     })
 }
 
 
+
+
+
 const book1 = new Book("Harry Pooper", "J.K. Roller", 369, false);
 const book2 = new Book("Of Mac and Cheese", "M.C. Donald", 144, true);
-const book3 = new Book("Count to 2", "Gaben", 2, false);
+const book3 = new Book("A reeeeeeeeally long title for a book", "A reeeeeeally long name like weewoo", 2, false);
 
 addBook(book1);
 addBook(book2);
